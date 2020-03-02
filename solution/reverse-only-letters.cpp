4f799ff8ac874bf1c39e9bb470f43f31790e1384
Nikolay Dimitrow <nikidimitrow@users.noreply.github.com>
2020-03-02 11:30:21 +0200

    Update reverse-only-letters.cpp

--- solution/reverse-only-letters.cpp
+++ solution/reverse-only-letters.cpp
@@ -1,1 +1,25 @@
 /* Link to the problem: https://leetcode.com/problems/reverse-only-letters/ */
+
+#include <string>
+
+using std::string;
+using std::swap;
+
+class Solution {
+public:
+    std::string reverseOnlyLetters(string S) {
+        int i = 0, j = S.length() - 1;
+        
+        while (i < j) {
+            if (isalpha(S[i]) && isalpha(S[j])) {
+                swap(S[i++], S[j--]);
+                continue;
+            }
+            if (!isalpha(S[i]))
+                i++;
+            if (!isalpha(S[j]))
+                j--;
+        } 
+        return S;
+    }
+};
