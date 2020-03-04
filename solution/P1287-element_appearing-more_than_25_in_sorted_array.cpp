d438aea134a058a95d21ed360ea9f7e619b07c22
Nikolay Dimitrow <nikidimitrow@users.noreply.github.com>
2020-03-04 10:31:11 +0200

    Create P1287-element_appearing-more_than_25_in_sorted_array.cpp

---
+++ solution/P1287-element_appearing-more_than_25_in_sorted_array.cpp
@@ -0,0 +1,24 @@
+/* Problem: https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array */
+
+class Solution 
+{
+public:
+    int findSpecialInteger(std::vector<int>& arr) 
+    {
+        int max = arr.size() / 4; //get the 25% of the array
+        
+        int i = 0;
+        
+        while(i < arr.size()) {
+            int num = arr[i];
+            if(i + max < arr.size() && arr[i + max] == num) {
+                return num;
+            }
+            while(arr[i] == num){
+                i++;
+            }
+        }
+        
+        return 1;
+    }
+};
