6f8acf70d1608f262f806f7d7bb3e2ccae69251a
Nikolay Dimitrow <nikidimitrow@users.noreply.github.com>
2020-03-07 13:27:49 +0200

    Update P0001-two_sum.cpp

--- solution/P0001-two_sum.cpp
+++ solution/P0001-two_sum.cpp
@@ -1,7 +1,21 @@
 /* Link to the problem: https://leetcode.com/problems/two-sum */
 
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        
-    }
-}
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+         vector<int> n;
+         for(int i = 0; i < nums.size(); i++)
+         {
+             for(int j = i + 1; j < nums.size(); j++)
+             {
+                 if(target == (nums[i] + nums[j]))
+                 {
+                     n.push_back(i);
+                     n.push_back(j);
+                 }
+             }
+         }
+      return n;
+    }  
+};
