8ae4fd727ae3613e78215a79fe9220e2870e396a
Nikolay Dimitrow <nikidimitrow@users.noreply.github.com>
2020-03-09 10:09:51 +0200

    Sollution of the problem

--- solution/P0905-sort-array-by-parity.cpp
+++ solution/P0905-sort-array-by-parity.cpp
@@ -3,6 +3,16 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& A) {
-        
+        int index = 0;
+        for(int i = 0; i < A.size(); i++)
+        {
+            if(A[i] % 2 == 0)
+            {
+                int temp = A[index];
+                A[index++] = A[i];
+                A[i] = temp;
+            }
+        }
+        return A;
     }
 };
