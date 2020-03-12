3300bdef94533fb5e2396294116f935b6e421019
Nikolay Dimitrow <nikidimitrow@users.noreply.github.com>
2020-03-12 14:46:00 +0200

    Solution

--- solution/P0014-longest-common-prefix.cpp
+++ solution/P0014-longest-common-prefix.cpp
@@ -1,1 +1,31 @@
 /* Link to the problem: https://leetcode.com/problems/longest-common-prefix */
+
+class Solution 
+{
+public:
+    string longestCommonPrefix(vector<string>& strs) 
+    {
+    std::string myNiki{};
+    for(auto i{0}; i < strs.size(); i++)
+    {
+        std::string niki = strs[0];
+        std::string niki1 = strs[1];
+        char c = niki[0];
+        char b = niki1[0];
+        
+      if(c == b)
+        {
+            char d = niki[1];
+            char e = niki1[1];
+           
+            if(d == e)
+            {
+                
+                return string nikiString.append(niki[0]).append(niki[1]);
+            }
+            
+        }
+        else return std::string myString{""};
+    }
+  }
+};
