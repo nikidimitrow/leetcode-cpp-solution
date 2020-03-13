4e4bc1a1d67f239efbc362eac5710a0e470b86d9
Nikolay Dimitrow <nikidimitrow@users.noreply.github.com>
2020-03-13 09:33:55 +0200

    Solution

--- solution/P0896-monotonic-array.cpp
+++ solution/P0896-monotonic-array.cpp
@@ -3,6 +3,8 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& A) {
-        
+       if((A[0] <= A[1]) && (A[1] <= A[2]))  return true;
+       if ((A[2] <= A[1]) && (A[1] <= A[0]))  return true;
+       else return false;       
     }
 };
